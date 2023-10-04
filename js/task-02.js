1b0e3d5ed130b96e54636e2729f54e1e40324281
AnnaStylee <136710517+AnnaStylee@users.noreply.github.com>
2023-10-04 12:07:14 +0300

    fix-task-2

--- js/task-02.js
+++ js/task-02.js
@@ -5,8 +5,6 @@
 // Додасть елементу клас item.
 // Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
 
-const ingredientsList = document.querySelector('#ingredients');
-
 const ingredients = [
   "Potatoes",
   "Mushrooms",
@@ -17,19 +15,18 @@ const ingredients = [
 ];
 
 
+// Знайдемо елемент ul#ingredients
+const ingredientsList = document.querySelector('#ingredients');
+
+// Створимо фрагмент
 const fragment = document.createDocumentFragment();
 
 ingredients.forEach((ingredient) => {
-
-const listItem = document.createElement('li');
-
-listItem.textContent = ingredient;
-
-listItem.classList.add('item');
-
-ingredientsList.appendChild(listItem);
-
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  fragment.appendChild(listItem); // Додаємо елемент <li> до фрагменту
 });
 
+// Вставляємо всі елементи з фрагменту в ul#ingredients
 ingredientsList.appendChild(fragment);
-
